c80f04c836213277a06e831e9a7682f100a4c7a1
Bujapetas <bujapetas@gmail.com>
2022-05-25 00:06:33 +0200

    Si?

--- EndRoundFunny/EndRoundFunny/Main.cs
+++ EndRoundFunny/EndRoundFunny/Main.cs
@@ -1,8 +1,24 @@
-﻿using Exiled.API.Features;
+﻿using System;
+using Exiled.API.Features;
 
 namespace EndRoundFunny
 {
     public class Main : Plugin<Config>
     {
+        public override string Name => typeof(Plugin<>).Namespace;
+        public override string Prefix => Name.ToLower();
+        public override string Author => "Buja";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(5, 2, 1);
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+        }
     }
 }
